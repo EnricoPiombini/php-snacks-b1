c3a26eb5cf565f21ad5c7ba27918e77d2b7d7e65
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-08 17:09:49 +0200

    nome minore di 3 caratteri

--- snack2/index.php
+++ snack2/index.php
@@ -4,3 +4,20 @@ name sia più lungo di 3 caratteri,
 che mail contenga un punto e una chiocciola e 
 che age sia un numero. 
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+<?php
+
+$name = key_exists('name', $_GET) ? trim($_GET['name']) : false;
+$mail = key_exists('mail', $_GET) ? trim($_GET['mail']) : false;
+$age = key_exists('age', $_GET) ? trim($_GET['age']) : false;
+
+// Controllo che name superi i 3 caratteri
+if(!$name){
+    echo "name not found";
+} else {
+    $nameIsvalid = true;
+    if(strlen($name) < 3){
+        $nameIsvalid = false;
+    }
+  var_dump($nameIsvalid);
+}
