0081100aefd600b3f9cb712ad406c4c1b6350e34
EnricoPiombini <piombini.enrico@gmail.com>
2022-07-11 10:44:58 +0200

    Snack 2 completo

--- snack2/index.php
+++ snack2/index.php
@@ -11,13 +11,52 @@ $name = key_exists('name', $_GET) ? trim($_GET['name']) : false;
 $mail = key_exists('mail', $_GET) ? trim($_GET['mail']) : false;
 $age = key_exists('age', $_GET) ? trim($_GET['age']) : false;
 
-// Controllo che name superi i 3 caratteri
-if(!$name){
-    echo "name not found";
-} else {
+$nameIsvalid = false;
+$mailIsvalid = false;
+$ageIsvalid = false;
+$dataValid = false;
+
+
+if (strlen($name) > 3) {
     $nameIsvalid = true;
-    if(strlen($name) < 3){
-        $nameIsvalid = false;
-    }
-  var_dump($nameIsvalid);
 }
+
+if (strpos($mail, "@") > 2 && strpos($mail, ".") > 4) {
+    $mailIsvalid = true;
+}
+
+if (is_numeric($age)) {
+    $ageIsvalid = true;
+}
+
+$dataValid = $nameIsvalid && $mailIsvalid && $ageIsvalid
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Email Validation</title>
+</head>
+
+<body>
+    <div class="container">
+        <?php if ($dataValid) {
+            echo "Accesso consentito";
+        } else {
+            echo "Accesso negato";
+        }
+        ?>
+    </div>
+
+
+</body>
+
+</html>
